2b2d13653de648897da042256f4a8dd96ab78946
Detroit98 <duruakuebuka@gmail.com>
2023-02-15 18:44:29 +0100

    navbar scrolling and hiding

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 const Layout = ({children}) => {
+  const [scrollTop, setScrollTop] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = event => {
+    const currentScrollTop = event.currentTarget.scrollTop;
+    setVisible(scrollTop > currentScrollTop);
+    setScrollTop(currentScrollTop);
+  };
   return (
-    <div className='layout'>
+    <div className='layout'
+    onScroll={handleScroll}
+    >
         <Head>
             <title>Detroit Store</title>
         </Head>
-        <header>
+        <header style={{ display: visible ? 'block' : 'none' }}>
             <Navbar />
         </header>
         
